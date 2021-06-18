727d7ddd133a2aaeef35380ab6b184eacdda57b3
Gairaud <51027552+Gairaud@users.noreply.github.com>
2021-06-18 02:55:29 -0600

    SWIPE

--- Lab05/app/src/main/java/com/cabegaira/lab05/CRUDStudent.kt
+++ Lab05/app/src/main/java/com/cabegaira/lab05/CRUDStudent.kt
@@ -2,15 +2,23 @@ package com.cabegaira.lab05
 
 import android.content.Intent
 import android.database.Cursor
+import android.graphics.Canvas
+import android.graphics.Color.red
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CRUDStudent : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
 
@@ -19,7 +27,8 @@ class CRUDStudent : AppCompatActivity(), NavigationView.OnNavigationItemSelected
     lateinit var list: RecyclerView
     lateinit var adapter : RecyclerView_Adapter_Students
     private var btn : Button? = null
-
+    lateinit var student:Students
+    var position: Int = 0
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.students_list)
@@ -47,7 +56,69 @@ class CRUDStudent : AppCompatActivity(), NavigationView.OnNavigationItemSelected
             startActivity(i)
         }
         listStudents()
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.START or ItemTouchHelper.END, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                val fromPosition: Int = viewHolder.adapterPosition
+                val toPosition: Int = target.adapterPosition
+
+
+
+                list.adapter?.notifyItemMoved(fromPosition, toPosition)
+
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                position = viewHolder.adapterPosition
+                var quien: String = ""
+
+
+                if(direction == ItemTouchHelper.LEFT){
+                    student = Students(studentsList[position].id, studentsList[position].name, studentsList[position].lastname,studentsList[position].age)
+                    //studentsList.deleteJob(position)
+                    list.adapter?.notifyItemRemoved(position)
+
+                    Snackbar.make(list, student.name + "Se eliminaría... ", Snackbar.LENGTH_LONG).setAction("Undo") {
+                        list.adapter?.notifyItemInserted(position)
+                    }.show()
+                    adapter = RecyclerView_Adapter_Students(studentsList)
+                    list.adapter = adapter
+                    adapter.notifyDataSetChanged()
+                }else{
+                    val intent = Intent(this@CRUDStudent, Login::class.java)
+                    val item = studentsList[position]
+                    intent.putExtra("dato", item )
+                    intent.putExtra("position",position)
+                    startActivity(intent)
+                    adapter.notifyDataSetChanged()
+                    //getListOfPersons()
+                }
+            }
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+
+                RecyclerViewSwipeDecorator.Builder(this@CRUDStudent, c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(this@CRUDStudent, R.color.red))
+                    .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                    .addSwipeRightBackgroundColor(ContextCompat.getColor(this@CRUDStudent, R.color.green))
+                    .addSwipeRightActionIcon(R.drawable.ic_baseline_edit_24)
+                    .create()
+                    .decorate()
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+
+        }
+
+
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(list)
+
+
     }
+    
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
